69fd5af527cf619c725e5a32fde9ee1ceab2249a
L0um15 <damian.ziolo@onet.pl>
2021-12-10 22:05:52 +0100

    Added MusixmatchService

---
+++ Spotisharp.Client/Services/MusixmatchService.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+
+namespace Spotisharp.Client.Services;
+
+public static class MusixmatchService
+{
+    private static readonly string _siteUrl = "https://www.musixmatch.com";
+    private static readonly string _searchUrl = "https://www.musixmatch.com/search/{0}/tracks";
+
+    public static async Task<string> SearchLyricsFromText(string query)
+    {
+        string searchQuery = String.Format(_searchUrl, query);
+        using (HttpClient client = new HttpClient())
+        {
+            string response = await client.GetStringAsync(searchQuery);
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(response);
+            
+            var foundList = htmlDoc.DocumentNode.SelectSingleNode("//ul[contains(@class, 'tracks')]");
+            
+            if(foundList == null)
+            {
+                return string.Empty;
+            }
+
+            var foundTrack = foundList.SelectSingleNode("//a[@class='title'][1]");
+
+            if(foundTrack == null)
+            {
+                return string.Empty;
+            }
+
+            string trackUrl = _siteUrl + foundTrack.Attributes["href"].Value;
+
+            response = await client.GetStringAsync(trackUrl);
+
+            htmlDoc.LoadHtml(response);
+
+            var lyrics = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='mxm-lyrics']/span"); // Approved Lyrics
+            if (lyrics == null)
+                lyrics = htmlDoc.DocumentNode.SelectSingleNode("//span[@class='lyrics__content__ok']"); // Correct Lyrics waiting for approval
+            if (lyrics == null)
+                lyrics = htmlDoc.DocumentNode.SelectSingleNode("//span[@class='lyrics__content__warning']"); // Incorrect Lyrics waiting for review.
+
+            return lyrics.InnerText ?? string.Empty;
+        }
+    }
+}
