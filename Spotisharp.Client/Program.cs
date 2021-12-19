f6e85ce01d751c8871fec75be4672cf4578e39cc
L0um15 <damian.ziolo@onet.pl>
2021-12-19 02:34:05 +0100

    Added FFmpegResolver, Added Spotisharp startup logo

--- Spotisharp.Client/Program.cs
+++ Spotisharp.Client/Program.cs
@@ -4,10 +4,56 @@ using Spotisharp.Client.Models;
 using Spotisharp.Client.Resolvers;
 using Spotisharp.Client.Services;
 using System.Collections.Concurrent;
+using System.Reflection;
 
-if (ConfigManager.Init())
+Console.WriteLine(@"
+               ▓        ▓
+               ▓        ▓      ▄▄
+         ▄▄▄▓▓▓▓███▓▓▓▓▓▓▄▄▓▓▀▀▀
+        ████████████████▓█████▄▄
+            ▄▄▄▓▀▀▀     ▓ ▀▀█████
+       ▄▓▓████████████▓▓▓▄▄
+         ▀█▀▀▀▀▓▀▀▀▀▀▀▀██████▄
+             ▄▄▓▄▄▄▄▄   ▓  ▀▀█▀
+          █████▓█▀██████▓▓▄
+               ▓        ▓▀██
+               ▓        ▓
+               ▓        ▓
+               ▀        ▀                     
+    ");
+
+Console.WriteLine("Spotisharp v" + Assembly.GetExecutingAssembly().GetName().Version.ToString() + 
+    "\n\tCopyright \u00a92021 Damian Ziolo\n");
+
+if (!FFmpegResolver.IsFFmpegInstalled())
 {
-    Console.WriteLine("Loaded config file!");
+    Console.WriteLine("Error: FFmpeg is missing.");
+    return;
+}
+
+if (!ConfigManager.Init())
+{
+    Console.WriteLine("Error: Couldn't load or create configuration file.");
+    return;
+}
+
+if (ConfigManager.Properties.IsFirstTime)
+{
+    Console.WriteLine("Hi!, Thank you for using Spotisharp!\n" +
+        "This message will be displayed only once\n" +
+        "Spotisharp v3 is now using PKCETokenAuthentication which is linked to your personal Spotify account\n" +
+        "For safety reasons please make sure you obtained this software from original repository\n" +
+        "Spotisharp is free and opensource. So go ahead and read its source code\n" +
+        "If you have any questions, go ahead and ask me on github\n" +
+        "Github: https://github.com/L0um15/Spotisharp \n");
+    ConfigManager.Properties.IsFirstTime = false;
+    ConfigManager.WriteChanges();
+}
+
+if (args.Length == 0)
+{
+    Console.WriteLine("No arguments provided. Exiting.");
+    return;
 }
 
 SpotifyClient? client = await SpotifyAuthentication.CreateSpotifyClient();
---
+++ Spotisharp.Client/Resolvers/FFmpegResolver.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+public static class FFmpegResolver
+{
+    public static bool IsFFmpegInstalled()
+    {
+        try
+        {
+            using (Process ffmpegProc = new Process())
+            {
+                ffmpegProc.StartInfo.FileName = "ffmpeg";
+                ffmpegProc.StartInfo.RedirectStandardError = true;
+                ffmpegProc.StartInfo.UseShellExecute = false;
+                ffmpegProc.StartInfo.CreateNoWindow = true;
+                ffmpegProc.Start();
+                ffmpegProc.WaitForExit();
+            };
+            return true;
+        }
+        catch (Win32Exception)
+        {
+            return false;
+        }
+        
+    }
+}
