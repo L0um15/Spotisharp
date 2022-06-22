436961e43b619695b7ecf65dd501a8e342be427b
alltdl <alltdl@gmail.com>
2022-06-21 21:02:40 -0600

    Fixed missing tag 'genre' and also fixed bug 19.

--- Spotisharp.Client/Services/SpotifyService.cs
+++ Spotisharp.Client/Services/SpotifyService.cs
@@ -50,6 +50,7 @@ public static class SpotifyService
         if(track != null)
         {
             FullAlbum album = await client.Albums.TryGet(track.Album.Id);
+            FullArtist artist = await client.Artists.TryGet(album.Artists[0].Id);
             TrackInfoModel trackInfo = new TrackInfoModel()
             {
                 Artist = track.Artists[0].Name,
@@ -61,7 +62,7 @@ public static class SpotifyService
                 Album = album.Name,
                 AlbumPicture = album.Images[0].Url,
                 Copyright = album.Copyrights.FirstOrDefault()?.Text ?? string.Empty,
-                Genres = album.Genres.FirstOrDefault() ?? string.Empty,
+                Genres = artist.Genres.FirstOrDefault() ?? string.Empty,
                 Year = DateTime.TryParse(album.ReleaseDate, out var value) ? value.Year : int.Parse(album.ReleaseDate)
             };
             bag.Add(trackInfo);
@@ -83,7 +84,7 @@ public static class SpotifyService
     }
 
     public static async Task PackPlaylistTracks(
-        SpotifyClient client, 
+        SpotifyClient client,
         string input,
         ConcurrentBag<TrackInfoModel> bag
     )
@@ -94,9 +95,10 @@ public static class SpotifyService
         {
             await foreach(var item in client.Paginate(playlist.Tracks))
             {
-                if (item.Track is FullTrack track)
+                if (item.Track is FullTrack track && !string.IsNullOrEmpty(track.Album.Id))
                 {
                     FullAlbum album = await client.Albums.TryGet(track.Album.Id);
+                    FullArtist artist = await client.Artists.TryGet(album.Artists[0].Id);
                     TrackInfoModel trackInfo = new TrackInfoModel()
                     {
                         Artist = track.Artists[0].Name,
@@ -108,7 +110,7 @@ public static class SpotifyService
                         Album = album.Name,
                         AlbumPicture = album.Images[0].Url,
                         Copyright = album.Copyrights.FirstOrDefault()?.Text ?? string.Empty,
-                        Genres = album.Genres.FirstOrDefault() ?? string.Empty,
+                        Genres = artist.Genres.FirstOrDefault() ?? string.Empty,
                         Year = DateTime.TryParse(album.ReleaseDate, out var value) ? value.Year : int.Parse(album.ReleaseDate)
                     };
                     bag.Add(trackInfo);
@@ -123,13 +125,14 @@ public static class SpotifyService
     }
 
     public static async Task PackAlbumTracks(
-        SpotifyClient client, 
-        string input, 
+        SpotifyClient client,
+        string input,
         ConcurrentBag<TrackInfoModel> bag
     )
     {
         int topCursorPosition = Console.CursorTop;
         FullAlbum album = await client.Albums.Get(input);
+        FullArtist artist = await client.Artists.TryGet(album.Artists[0].Id);
         await foreach (SimpleTrack track in client.Paginate(album.Tracks))
         {
             TrackInfoModel trackInfo = new TrackInfoModel()
@@ -143,7 +146,7 @@ public static class SpotifyService
                 Album = album.Name ?? string.Empty,
                 AlbumPicture = album.Images[0].Url,
                 Copyright = album.Copyrights.FirstOrDefault()?.Text ?? string.Empty,
-                Genres = album.Genres.FirstOrDefault() ?? string.Empty,
+                Genres = artist.Genres.FirstOrDefault() ?? string.Empty,
                 Year = DateTime.TryParse(album.ReleaseDate, out var value) ? value.Year : int.Parse(album.ReleaseDate)
             };
             bag.Add(trackInfo);
@@ -158,16 +161,31 @@ public static class SpotifyService
     private static async Task<FullAlbum> TryGet(this IAlbumsClient client, string albumid)
     {
         FullAlbum album;
-        TryAgain:
-            try
-            {
-                album = await client.Get(albumid);
-                return album;
-            }
-            catch (APITooManyRequestsException ex)
-            {
-                await Task.Delay((int)(ex.RetryAfter.TotalMilliseconds));
-                goto TryAgain;
-            }
+    TryAgain:
+        try
+        {
+            album = await client.Get(albumid);
+            return album;
+        }
+        catch (APITooManyRequestsException ex)
+        {
+            await Task.Delay((int)(ex.RetryAfter.TotalMilliseconds));
+            goto TryAgain;
+        }
+    }
+
+    private static async Task<FullArtist> TryGet(this IArtistsClient client, string artistid)
+    {
+    TryAgain:
+        try
+        {
+            FullArtist _ = await client.Get(artistid);
+            return _;
+        }
+        catch (APITooManyRequestsException ex)
+        {
+            await Task.Delay((int)(ex.RetryAfter.TotalMilliseconds));
+            goto TryAgain;
+        }
     }
 }
