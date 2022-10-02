85ad7909da228967207f1a8132a604ac8e966b48
L0um15 <damian.ziolo@onet.pl>
2022-10-02 02:32:20 +0200

    Add checks to FilenameResolverTests

--- Spotisharp.Tests/ResolversTests/FilenameResolverTests.cs
+++ Spotisharp.Tests/ResolversTests/FilenameResolverTests.cs
@@ -1,4 +1,5 @@
-﻿using Spotisharp.Client.Resolvers;
+﻿using Spotisharp.Client.Enums;
+using Spotisharp.Client.Resolvers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,17 @@ namespace Spotisharp.Tests.ResolversTests
     public class FilenameResolverTests
     {
         [Theory]
-        [InlineData("\"sad\"", "sad")]
-        [InlineData("[|DOMINO|]", "[DOMINO]")]
-        [InlineData("*life is a diamond*", "life is a diamond")]
-        [InlineData("Would you rather?", "Would you rather")]
-        [InlineData("lost cause// (feat. Jesse)", "lost cause (feat. Jesse)")]
-        [InlineData("Nightcore & KYANU Edit", "Nightcore KYANU Edit")]
-        public void RemoveForbiddenChars_SpecialCharactersShouldBeRemoved(string filename, string expected)
+        [InlineData("\"sad\"", "sad", StringType.Filename)]
+        [InlineData("[|DOMINO|]", "[DOMINO]", StringType.Filename)]
+        [InlineData("*life is a diamond*", "life is a diamond", StringType.Filename)]
+        [InlineData("Would you rather?", "Would you rather", StringType.Filename)]
+        [InlineData("lost cause// (feat. Jesse)", "lost cause (feat. Jesse)", StringType.Filename)]
+        [InlineData("Nightcore & KYANU Edit", "Nightcore & KYANU Edit", StringType.Filename)]
+
+        [InlineData("TEST&+,;@$%#!=<>:\"/\\|?* PASSED", "TEST PASSED",StringType.Url)]
+        public void RemoveForbiddenChars_SpecialCharactersShouldBeRemoved(string filename, string expected, StringType sType)
         {
-            var cleansed_filename = FilenameResolver.RemoveForbiddenChars(filename);
+            var cleansed_filename = FilenameResolver.RemoveForbiddenChars(filename, sType);
             Assert.Equal(expected, cleansed_filename);
         }   
     }
